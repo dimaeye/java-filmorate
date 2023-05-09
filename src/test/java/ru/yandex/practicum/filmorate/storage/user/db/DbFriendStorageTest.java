3502ba976694019e4055c288532d28bc56811be7
dimaeye <dima.airman23@yandex.ru>
2023-05-10 02:34:09 +0300

    test: added integration database test of users

--- src/test/java/ru/yandex/practicum/filmorate/storage/user/db/DbFriendStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/storage/user/db/DbFriendStorageTest.java
@@ -1,20 +1,70 @@
 package ru.yandex.practicum.filmorate.storage.user.db;
 
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.time.LocalDate;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@AutoConfigureTestDatabase
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
 class DbFriendStorageTest {
+    private final DbFriendStorage dbFriendStorage;
+    private final DbUserStorage dbUserStorage;
 
     @Test
     void getAllFriends() {
+        int firstUserId = dbUserStorage.add(getNewUser());
+        int secondUserId = dbUserStorage.add(getNewUser());
+
+        dbFriendStorage.addFriend(firstUserId, secondUserId);
+
+        assertAll(
+                () -> assertEquals(secondUserId, dbFriendStorage.getAllFriends(firstUserId).get(0).getId()),
+                () -> assertTrue(dbFriendStorage.getAllFriends(secondUserId).isEmpty())
+        );
     }
 
     @Test
     void addFriend() {
+        int firstUserId = dbUserStorage.add(getNewUser());
+        int secondUserId = dbUserStorage.add(getNewUser());
+
+        dbFriendStorage.addFriend(firstUserId, secondUserId);
+
+        assertEquals(secondUserId, dbFriendStorage.getAllFriends(firstUserId).get(0).getId());
     }
 
     @Test
     void deleteFriend() {
+        int firstUserId = dbUserStorage.add(getNewUser());
+        int secondUserId = dbUserStorage.add(getNewUser());
+
+        assertAll(
+                () -> {
+                    dbFriendStorage.addFriend(firstUserId, secondUserId);
+                    assertEquals(secondUserId, dbFriendStorage.getAllFriends(firstUserId).get(0).getId());
+
+                },
+                () -> {
+                    dbFriendStorage.deleteFriend(firstUserId, secondUserId);
+                    assertTrue(dbFriendStorage.getAllFriends(firstUserId).isEmpty());
+                }
+        );
+    }
+
+    private User getNewUser() {
+        User user = new User(
+                "mail@mail.ru", UUID.randomUUID().toString(), LocalDate.now()
+        );
+        user.setName("testName");
+        return user;
     }
 }
--- src/test/java/ru/yandex/practicum/filmorate/storage/user/db/DbUserStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/storage/user/db/DbUserStorageTest.java
@@ -1,28 +1,84 @@
 package ru.yandex.practicum.filmorate.storage.user.db;
 
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@AutoConfigureTestDatabase
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
 class DbUserStorageTest {
+    private final DbUserStorage dbUserStorage;
+    private static User user;
+
+    @BeforeAll
+    public static void setUp() {
+        user = new User(
+                "mail@mail.ru", "dolore", LocalDate.now()
+        );
+        user.setName("testName");
+    }
 
     @Test
     void add() {
+        user.setLogin(UUID.randomUUID().toString());
+        assertDoesNotThrow(
+                () -> dbUserStorage.add(user)
+        );
     }
 
     @Test
     void get() {
+        user.setLogin(UUID.randomUUID().toString());
+        int userId = dbUserStorage.add(user);
+
+        assertDoesNotThrow(
+                () -> dbUserStorage.get(userId)
+        );
     }
 
     @Test
     void getAll() {
+        user.setLogin(UUID.randomUUID().toString());
+        int userId = dbUserStorage.add(user);
+        List<User> allUsers = dbUserStorage.getAll();
+
+        assertTrue(allUsers.stream().filter(u -> u.getId() == userId).findFirst().isPresent());
     }
 
     @Test
     void update() {
+        user.setLogin(UUID.randomUUID().toString());
+        int userId = dbUserStorage.add(user);
+
+        User addedUser = dbUserStorage.get(userId);
+        addedUser.setEmail("new@Email.ru");
+        addedUser.setName("new Name");
+
+        User updatedUser = dbUserStorage.update(addedUser);
+
+        assertAll(
+                () -> assertEquals(addedUser.getEmail(), updatedUser.getEmail()),
+                () -> assertEquals(addedUser.getName(), updatedUser.getName())
+        );
     }
 
     @Test
     void delete() {
+        int userId = dbUserStorage.add(user);
+
+        assertDoesNotThrow(
+                () -> dbUserStorage.delete(userId)
+        );
     }
 }
